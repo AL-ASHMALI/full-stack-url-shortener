3145d2d1090e1a5c9440be6da5af8b1b6a4e957a
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2023-05-13 12:08:05 +0100

    adding automaited testing using puppeteer

---
+++ index.test.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+test('should input a long URL and return a short URL', async() => {
+  const browser = await puppeteer.launch({
+    headless: false, 
+    slowMode: 1000
+
+  }); // launch the browser 
+  const page = await browser.newPage(); // create a new page
+  await page.goto("http://localhost:3000/");
+  await page.click("input#fullUrl"); // selecting the input field and clicking on it. 
+  await page.type('input#fullUrl', "https://github.com/AL-ASHMALI");
+  await page.click('button');
+
+
+// await browser.close(); 
+})
